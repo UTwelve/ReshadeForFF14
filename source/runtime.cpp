813a89a11385b29051737fd7ab6506b548c9e2a2
utwelve <12haikui@gmail.com>
2020-02-06 08:39:06 +0800

    Network和Input.cpp编码

--- source/runtime.cpp
+++ source/runtime.cpp
@@ -250,7 +250,8 @@ void reshade::runtime::on_present()
 	}
 	else
 	{
-		_has_high_network_activity = traffic > 10;
+		//_has_high_network_activity = traffic > 10;
+		_has_high_network_activity = false;
 		traffic = 0;
 		cooldown = 60;
 	}
